f90a167f04ecc7f2058b55d802a9fa6b3e0fd06b
Neozaru <Neozaru@mailoo.org>
2012-10-02 10:24:21 +0200

    &

---
+++ rmirc/src/rmirc/Interfaces/InterfaceServeurForum.java
@@ -0,0 +1,12 @@
+package rmirc.Interfaces;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+
+public interface InterfaceServeurForum extends Remote {
+
+	public InterfaceSujetDiscussion obtientSujet( String titre_sujet ) throws RemoteException;
+	
+	public boolean enregistreSujet( InterfaceSujetDiscussion sujet ) throws RemoteException;
+	
+}
---
+++ rmirc/src/rmirc/Interfaces/InterfaceSujetDiscussion.java
@@ -0,0 +1,18 @@
+package rmirc.Interfaces;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+
+public interface InterfaceSujetDiscussion extends Remote {
+
+	public void inscription( InterfaceAffichageClient iface_client ) throws RemoteException;
+	
+	public void desInscription( InterfaceAffichageClient iface_client ) throws RemoteException;
+	
+	public void diffuse( String message ) throws RemoteException;
+	
+	public String get_titre() throws RemoteException;
+	
+	
+}
